13012591024e144e6ed1eb59b2151b6e7536ed96
Ryan Helsing <ryanhelsing@gmail.com>
2017-06-24 20:20:35 -0700

    split length aquired

--- mm.rb
+++ mm.rb
@@ -1,12 +1,19 @@
 require "date"
-@interval = 4.0
-@offset = 0.0
+require "pry"
+@interval = 10.0 #no clip will be longer than this.. double if needed
+@bpm = 96.0 #same as interval
+@offset = 0.732
+@frame_shift = 0.93#not yet
+@length = 3.51 #in minutes.seconds
 @folder = "/Users/ryanhelsing/Movies/Mexico"
+@song = "/Users/ryanhelsing/Music/cool_blue.mp3"
 @files = Dir["#{@folder}/*"]
 
 #get length of song
 
 # puts @files
+
+#convert length to seconds
 
 #gather info for each file, load into hash and sort
 @hash = {}
@@ -14,8 +21,20 @@ require "date"
 @files.each do |f|
   length = %x(ffprobe -i #{f} -show_entries format=duration -v quiet -of csv="p=0")
   created = %x(ffprobe -v quiet #{f} -print_format json -show_entries format_tags=creation_time -of csv="p=0")
-  @hash[f] = {length: length.gsub("\n", "").strip, date: DateTime.parse("#{created.gsub(" ", "T")}+0:00")}
+  @hash[f] = {length: length.gsub("\n", "").strip.to_f, date: DateTime.parse("#{created.gsub(" ", "T")}+0:00")}
 end
 
-#go through hash and determine optimal length of each video based on interval
-puts @hash
+#go through hash and determine optimal length of each video based on interval - round down to nearest 1/(2^n) of @interval
+@hash.each do |k, v|
+  temp_strip_length = @interval
+  power = 0
+  while temp_strip_length > v[:length]
+    power += 1
+    temp_strip_length = @interval*(1/(2**power.to_f))
+  end
+  v[:split_length] = temp_strip_length
+end
+
+@hash.each{|r| p r }
+
+#add up all split lengths
