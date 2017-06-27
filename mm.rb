5bca4337d9ccfc2ccf2751617e3d1baf60941f57
Ryan Helsing <ryanhelsing@gmail.com>
2017-06-27 13:31:58 -0700

    most recent

--- mm.rb
+++ mm.rb
@@ -2,7 +2,7 @@ require "date"
 require "pry"
 @interval = 10.0 #no clip will be longer than this.. double if needed
 @bpm = 96.0 #same as interval
-@offset = 0.732
+@offset = 1.250
 @frame_shift = 0.93#not yet
 @length = "3:51" #in minutes.seconds
 @folder = "/Users/ryanhelsing/Movies/Mexico"
@@ -13,6 +13,8 @@ require "pry"
 #get length of song
 
 # puts @files
+
+#fix all rotation at beginning.. once, in place?
 
 #convert length to seconds
 
@@ -71,7 +73,11 @@ puts "slicing"
 i = 0
 @hash.each do |k, v|
   if v[:rotate]
-    %x(ffmpeg -i #{k} -ss 0.00 -t #{v[:split_length]} -r 60 -qscale 0 -acodec copy #{@output}/temp_#{i}.mp4) #need a faster way :( - autorotate w/ metadata loses on concat
+    # Note: All inputs must have the same stream types (same formats, same time base, etc.).
+    # ffmpeg -i <input_filename> -vf "transpose=<dir>" -metadata:s:v "rotate=0" -r 60 -vcodec libx264 -crf 18 -acodec copy <output_filename>
+    # Stream #0:0(und): Video: h264 (High) (avc1 / 0x31637661), yuv420p(tv, bt709), 1920x1080, 23735 kb/s, 60 fps, 60 tbr, 600 tbn, 1200 tbc (default)
+    # %x(ffmpeg -i #{k} -ss 0.00 -t #{v[:split_length]} -r 60 -vcodec libx264 -crf 18 -acodec copy #{@output}/temp_#{i}.mp4) #need a faster way :( - autorotate w/ metadata loses on concat
+    %x(ffmpeg -i #{k} -c copy -ss 0.00 -t #{v[:split_length]} #{@output}/temp_#{i}.mp4)
   else
     %x(ffmpeg -i #{k} -c copy -ss 0.00 -t #{v[:split_length]} #{@output}/temp_#{i}.mp4)
   end
@@ -99,7 +105,9 @@ File.open("#{@output}/input.txt", "w+") do |f|
   @hash_output.each { |k, v| f.puts("file '#{k.gsub("#{@output}/", "")}'") }
 end
 
-%x(ffmpeg -f concat -i #{@output}/input.txt -c copy -safe 0 #{@output}/output.mp4)
+%x(ffmpeg -f concat -i #{@output}/input.txt -c copy #{@output}/output.mp4)
+# binding.pry
+# %x(ffmpeg -i "concat:#{@hash_output.map{|x| x[0]}.join("|")}" -c copy #{@output}/output.mp4)
 
 #compare length of song vs vid to output which is longer and by how much
 vid_length = @hash.map{|x| x[1][:split_length] }.reduce(:+)
@@ -115,9 +123,9 @@ puts "adding audio"
 %x(ffmpeg -i #{@output}/output.mp4 -i #{@song} -c:v copy -map 0:v:0 -map 1:a:0 -shortest #{@output}/output_final.mp4)
 
 puts "cleanup"
-%x(rm #{@output}/output.mp4)
-%x(rm #{@output}/temp_*)
-%x(rm #{@output}/*.txt)
+# %x(rm #{@output}/output.mp4)
+# %x(rm #{@output}/temp_*)
+# %x(rm #{@output}/*.txt)
 
 puts "done"
 #cleanup
